8f369a3b3669adeee090797ff8a9867a026e2811
Egor Artemov <egor.artemov@gmail.com>
2015-07-03 18:30:44 +0300

    Use MSVS error format for tests if built by VC

--- tests/minunit.h
+++ tests/minunit.h
@@ -1,6 +1,13 @@
 /* file: minunit.h */
 #define mu_stringify(s) #s
+#ifdef _MSC_VER
+#define mu_fileline(line) __FILE__ "(" mu_stringify( line ) ")"
+#define mu_assert(test) do { if (!(test)) return mu_fileline(__LINE__) ": error MU001: " mu_stringify(test)  "\n"; } while (0)
+#define mu_run_test(test) do { const char *message = test(); \
+    if (message) { fprintf(stderr, "%s", message);} } while (0)
+#else
 #define mu_fileline(line) __FILE__ ":" mu_stringify( line )
 #define mu_assert(test) do { if (!(test)) return mu_fileline(__LINE__) ": assert: " mu_stringify(test)  "\n"; } while (0)
 #define mu_run_test(test) do { const char *message = test(); \
     if (message) { fprintf(stderr, "%s", message);} } while (0)
+#endif
