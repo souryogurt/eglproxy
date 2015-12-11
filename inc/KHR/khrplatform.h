805fae5a17d76b4daa33c8dc5225486dcb4dfadb
Egor Artemov <egor.artemov@gmail.com>
2015-12-11 16:38:01 +0300

    Make it possible to link eglproxy statically on Win32

--- inc/KHR/khrplatform.h
+++ inc/KHR/khrplatform.h
@@ -98,7 +98,11 @@
  * This precedes the return type of the function in the function prototype.
  */
 #if defined(_WIN32) && !defined(__SCITECH_SNAP__)
-#   define KHRONOS_APICALL __declspec(dllimport)
+#   if defined(EGLPROXY_STATIC)
+#      define KHRONOS_APICALL
+#   else
+#      define KHRONOS_APICALL __declspec(dllimport)
+#   endif
 #elif defined (__SYMBIAN32__)
 #   define KHRONOS_APICALL IMPORT_C
 #else
